eccd62cf9e28ab513f88a4c54609050bade0f2ce
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-03-13 16:23:21 -0300

    feat: add imc calc

--- app/src/main/java/com/harrrisonhenri/imccalculator/MainActivity.kt
+++ app/src/main/java/com/harrrisonhenri/imccalculator/MainActivity.kt
@@ -2,10 +2,31 @@ package com.harrrisonhenri.imccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT?.text.toString(), alturaEDT?.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            val toast = Toast.makeText(applicationContext, "Seu resultado é %.2f".format(imc), Toast.LENGTH_LONG)
+            toast.show()
+        }
     }
 }
